fddb2a3e9ebb8650efc78d955987e15f46926141
Christina Gonzalez <cmgamway@gmail.com>
2019-08-09 02:58:02 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -8,11 +8,13 @@ def "CLI Jukebox"
 
   end
 
-  def list.each_with_index
+  def list
+    :each_with_index {
     puts list of songs you can play
-    songs => ["Phoenix - 1901", "Tokyo Police Club - Wait Up", "Sufjan Stevens - Too Much", "The Naked and the Famous - Young Blood",
-      "(Far From) Home - Tiga", "The Cults - Abducted", "Phoenix - Consolation Prizes", "Harry Chapin - Cats n the Cradle",
-      "Amos Lee - Keep It Loose, Keep It Tight"]
+    songs => [1."Phoenix - 1901", 2. "Tokyo Police Club - Wait Up", 3. "Sufjan Stevens - Too Much", 4. "The Naked and the Famous - Young Blood",
+      5. "(Far From) Home - Tiga", 6. "The Cults - Abducted", "Phoenix - Consolation Prizes", 7. "Harry Chapin - Cats n the Cradle",
+      8. "Amos Lee - Keep It Loose, Keep It Tight"]
+    }
   end
 
   def play
@@ -22,3 +24,17 @@ def "CLI Jukebox"
       if "user's response is invalid" do
         puts "Invalid input, please try again"
     end
+
+    def exit
+      puts "Goodbye"
+
+    end
+
+    def run
+      if "user selects Help use help.method" do
+        if "user selects List print the list of songs to play" do
+          if "user selects Play prompt user to select a song" do
+            if "user selects exit put 'Goodbye' and exit application"
+
+            end
+          end
