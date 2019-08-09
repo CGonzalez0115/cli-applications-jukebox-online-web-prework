241e72a78cfc3d038bd70a4082a01e345ec84293
Christina Gonzalez <cmgamway@gmail.com>
2019-08-09 02:37:19 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -8,18 +8,12 @@ def "CLI Jukebox"
 
   end
 
-  def list
+  def list.each_with_index
     puts list of songs you can play
-    :songs => [
-      "Phoenix - 1901",
-      "Tokyo Police Club - Wait Up",
-      "Sufjan Stevens - Too Much",
-      "The Naked and the Famous - Young Blood",
-      "(Far From) Home - Tiga",
-      "The Cults - Abducted",
-      "Phoenix - Consolation Prizes",
-      "Harry Chapin - Cats in the Cradle",
-      "Amos Lee - Keep It Loose, Keep It Tight"
-    ]
+    songs => ["Phoenix - 1901", "Tokyo Police Club - Wait Up", "Sufjan Stevens - Too Much", "The Naked and the Famous - Young Blood", 
+      "(Far From) Home - Tiga", "The Cults - Abducted", "Phoenix - Consolation Prizes", "Harry Chapin - Cats n the Cradle", 
+      "Amos Lee - Keep It Loose, Keep It Tight"]
   end
-  
+
+  def play
+    
