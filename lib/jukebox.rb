67e0978b249592b54d0dbc48dc70b6b2f5452e08
Christina Gonzalez <cmgamway@gmail.com>
2019-08-09 02:22:56 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,9 +1,33 @@
 # Add your code here
-def say_hello(name)
-  "Hi #{name}!"
+```bash
+I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program
+```
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+
+
+run(songs)
+def "CLI Jukebox"
+  def help
+    puts help : displays this help message
+    puts list : displays a list of songs you can play
+    puts play : lets you choose a song to play
+    puts exit : exits this program
+
+  end
 end
-
-puts "Enter your name:"
-users_name = gets.strip
-
-puts say_hello(users_name)
+    
